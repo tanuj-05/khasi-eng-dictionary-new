769c609daa2e5fed8a0386fb4cc18550c3a04046
Leon Patrick Mawlong <leonmawlong@gmail.com>
2019-04-13 14:18:38 +0530

    updated Activity main by removing redundant imports

--- app/src/main/java/com/example/android/justtrying/MainActivity.java
+++ app/src/main/java/com/example/android/justtrying/MainActivity.java
@@ -1,44 +1,17 @@
 package com.example.android.justtrying;
 
-import android.content.Intent;
 
 import android.os.Bundle;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
-import android.support.annotation.NonNull;
+
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
+
 import android.view.MenuItem;
-import android.view.View;
 
-import android.widget.Button;
-import android.widget.EditText;
-
-import android.widget.TextView;
-import android.widget.Toast;
-
-
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentChange;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.annotation.Nullable;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -91,6 +64,38 @@ public class MainActivity extends AppCompatActivity{
 }
 
 
+
+
+//import android.support.v7.widget.LinearLayoutManager;
+//        import android.support.v7.widget.RecyclerView;
+//        import android.util.Log;
+//import android.view.View;
+//
+//        import android.content.Intent;
+//        import android.widget.Button;
+//        import android.widget.EditText;
+//
+//        import android.widget.TextView;
+//        import android.widget.Toast;
+//
+//
+//        import com.google.android.gms.tasks.OnFailureListener;
+//        import com.google.android.gms.tasks.OnSuccessListener;
+//
+//        import com.google.firebase.firestore.CollectionReference;
+//        import com.google.firebase.firestore.DocumentChange;
+//        import com.google.firebase.firestore.DocumentReference;
+//        import com.google.firebase.firestore.EventListener;
+//        import com.google.firebase.firestore.FirebaseFirestore;
+//        import com.google.firebase.firestore.FirebaseFirestoreException;
+//        import com.google.firebase.firestore.QueryDocumentSnapshot;
+//        import com.google.firebase.firestore.QuerySnapshot;
+//
+//        import java.util.ArrayList;
+//        import java.util.List;
+//
+//        import javax.annotation.Nullable;
+//import android.support.annotation.NonNull;
 //public class MainActivity extends AppCompatActivity implements WordListAdapter.OnWordListener {
 //
 //
