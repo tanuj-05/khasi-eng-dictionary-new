a3a735f0fc4290d8748a14f2c0655e5b4a20cc69
tanuj-05 <tans.c2@gmail.com>
2019-04-13 17:05:55 +0530

    Loading words during splash screen implemented

---
+++ app/src/main/java/com/example/android/justtrying/KhasiToEnglish.java
@@ -0,0 +1,27 @@
+package com.example.android.justtrying;
+
+public class KhasiToEnglish {
+    private String khasi;
+    private String english;
+    public KhasiToEnglish(){}
+//    public KhasiToEnglish(String english, String khasi){
+//        this.khasi = khasi;
+//        this.english = english;
+//    }
+
+    public String getKhasi() {
+        return khasi;
+    }
+
+    public void setKhasi(String khasi) {
+        this.khasi = khasi;
+    }
+
+    public String getEnglish() {
+        return english;
+    }
+
+    public void setEnglish(String english) {
+        this.english = english;
+    }
+}
--- app/src/main/java/com/example/android/justtrying/SplashActivity.java
+++ app/src/main/java/com/example/android/justtrying/SplashActivity.java
@@ -1,22 +1,70 @@
 package com.example.android.justtrying;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.*;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
 public class SplashActivity extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 3000;
+    private static final String TAG = "SplashActivity";
+    ArrayList<String> khasiWords = new ArrayList<>();
+    ArrayList<String> englishWords = new ArrayList<>();
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference khasiToEnglish = db.collection("translations");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent i = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(i);
-                finish();
-            }
-        }, SPLASH_TIME_OUT);
+        LoadData loadData = new LoadData();
+        loadData.start();
+
+    }
+
+    class LoadData extends Thread {
+        @Override
+        public void run() {
+            super.run();
+            khasiToEnglish.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                @Override
+                public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                    for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                        KhasiToEnglish khasiToEnglish = documentSnapshot.toObject(KhasiToEnglish.class);
+                        String khasi = khasiToEnglish.getKhasi();
+                        String english = khasiToEnglish.getEnglish();
+                        khasiWords.add(khasi);
+                        englishWords.add(english);
+                    }
+                    loadingDone();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(SplashActivity.this, "Error in addOnFailure", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        private void loadingDone(){
+            Log.i(TAG, "first item in the arraylist is:" + khasiWords.get(0));
+            Log.i(TAG, "first item in the arraylist is:" + englishWords.get(0));
+            Intent i = new Intent(SplashActivity.this, MainActivity.class);
+            i.putStringArrayListExtra("khasi_words",khasiWords);
+            i.putStringArrayListExtra("english_words",englishWords);
+            Log.i(TAG, "onSuccess: " + khasiWords.size());
+            startActivity(i);
+            finish();
+        }
     }
 }
