3b3e32e771f4ef0349c3480886a30f6395268058
Leon Patrick Mawlong <leonmawlong@gmail.com>
2019-04-29 23:05:41 +0530

    Cleaned the code.

--- app/src/main/java/com/example/android/justtrying/FileHelperFavourite.java
+++ app/src/main/java/com/example/android/justtrying/FileHelperFavourite.java
@@ -25,38 +25,6 @@ public class FileHelperFavourite {
     private static final String FILENAME = "favinfo.dat";
     private static final String FILENAME2 = "recentsinfo.dat";
 
-//    public static void writeData(ArrayList<String> items, Context context) {
-//        try{
-//            FileOutputStream fos = context.openFileOutput(FILENAME,Context.MODE_PRIVATE);
-//            ObjectOutputStream oos = new ObjectOutputStream(fos);
-//            oos.writeObject(items);
-//            oos.close();
-//
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    public static ArrayList<String> readData(Context context) {
-//        ArrayList<String> itemList = null;
-//
-//        try {
-//            FileInputStream fis = context.openFileInput(FILENAME);
-//            ObjectInputStream ois = new ObjectInputStream(fis);
-//            itemList = (ArrayList<String>) ois.readObject();
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } catch (ClassNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//
-//        return itemList;
-//    }
-
 
     public static void writeDataRecents(String textToAdd, Context context) {
 
